241a1af7aaf308cbccc82e530ad437a2e5f31dda
vinayak1514 <vinayakjangir1514@gmail.com>
2023-10-29 09:02:03 +0530

    change in setup file

--- setup.py
+++ setup.py
@@ -17,8 +17,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name='DimondPricePrediction',
     version='0.0.1',
-    author='sunny savita',
-    author_email='sunny.savita@ineuron.ai',
+    author='vinayak',
+    author_email='jangirvinayak0001@gmail.com',
     install_requires=["scikit-learn","pandas","numpy"],
     packages=find_packages()
 )
