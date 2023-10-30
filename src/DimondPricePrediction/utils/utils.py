fefc732508e1f28e0ba37ef497c2f69037c5fe28
vinayak1514 <vinayakjangir1514@gmail.com>
2023-10-30 08:26:03 +0530

    do some change in utils file

--- src/DimondPricePrediction/utils/utils.py
+++ src/DimondPricePrediction/utils/utils.py
@@ -5,15 +5,25 @@ import pandas as pd
 import numpy as np
 from src.DimondPricePrediction.exception import CustomException 
 from src.DimondPricePrediction.logger import logging 
-
-def save_object(file_path,obj):
+def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-        os.makedirs(dir_path,exist_ok=True)
-        with open(file_path,'wb') as file_obj:
-            pickle.dump(obj,file_obj)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
     except Exception as e:
-        raise CustomException(e,sys)
+        raise customexception(e, sys)
+# def save_object(file_path,obj):
+#     try:
+#         dir_path = os.path.dirname(file_path)
+#         os.makedirs(dir_path,exist_ok=True)
+#         with open(file_path,'wb') as file_obj:
+#             pickle.dump(obj,file_obj)
+#     except Exception as e:
+#         raise CustomException(e,sys)
 def load_object(file_path):
     try:
         with open(file_path,'rb') as file_obj:
@@ -21,3 +31,4 @@ def load_object(file_path):
     except Exception as e:
         logging.info('Exception Occured in load_object function utils')
         raise customexception(e,sys)
+
