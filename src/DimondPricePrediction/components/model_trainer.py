bf97abc8a3e8431a8a42bd18d4b85c3651c10a63
vinayak1514 <vinayakjangir1514@gmail.com>
2023-10-30 10:19:53 +0530

    add model traniner file

--- src/DimondPricePrediction/components/model_trainer.py
+++ src/DimondPricePrediction/components/model_trainer.py
@@ -2,8 +2,53 @@ import os
 import sys 
 import pandas as pd 
 import numpy as np 
+from dataclasses import dataclass
 from src.DimondPricePrediction.logger import logging
 from src.DimondPricePrediction.exception import CustomException
 from src.DimondPricePrediction.utils.utils import save_object
-# from src.DimondPricePrediction.utils.utils import
+from src.DimondPricePrediction.utils.utils import evaluate_model
 from sklearn.linear_model import LinearRegression ,Ridge,Lasso,ElasticNet
+from sklearn.metrics import r2_score
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join('artifacts','model.pkl')
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config=ModelTrainerConfig()
+    
+    def initate_model_training(self,train_array,test_array):
+        try:
+            logging.info('Splitting Dependent and Independent variables from train and test data')
+            X_train,y_train,X_test,y_test = [
+                    train_array[:,:-1],
+                    train_array[:,-1],
+                    test_array[:,:-1],
+                    test_array[:,-1]
+            ]
+            models = {
+            'LinearRegression':LinearRegression(),
+            'Lasso':Lasso(),
+            'Ridge':Ridge(),
+            'Elasticnet':ElasticNet()
+            }
+            model_report:dict = evaluate_model(X_train,X_test,y_train,y_test,models)
+            print(model_report)
+            logging.info(f'Model Report : {model_report}')
+
+            best_model_score = max(model_report.values())
+            best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+
+            best_model = models[best_model_name]
+            logging.info(f'Best Model Found-Model Name : {best_model_name} , R2 Score : {best_model_score}')
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+
+
+        except Exception as e:
+            logging.info('error accured at initate model training ')
+            raise CustomException(e,sys)
--- src/DimondPricePrediction/pipelines/training_pipeline.py
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -6,11 +6,14 @@ from src.DimondPricePrediction.logger import logging
 from src.DimondPricePrediction.exception import CustomException
 from src.DimondPricePrediction.components.data_ingestion import DataIngestion
 from src.DimondPricePrediction.components.data_transformation import DataTransformation
-
+from src.DimondPricePrediction.components.model_trainer import ModelTrainer
 
 obj=DataIngestion()
 
 train_data_path,test_data_path=obj.initiate_data_ingestion()
 
 data_transformation=DataTransformation()
-data_transformation.initialize_data_transformation(train_data_path,test_data_path)
+train_arr , test_arr = data_transformation.initialize_data_transformation(train_data_path,test_data_path)
+
+model = ModelTrainer()
+model.initate_model_training(train_arr,test_arr)
