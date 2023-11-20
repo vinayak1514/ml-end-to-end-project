ed20dfd34f8c0f2551f4214de6eb7a5bd60d1537
vinayak1514 <vinayakjangir1514@gmail.com>
2023-11-20 14:30:17 +0530

    add model evaluation code

--- src/DimondPricePrediction/pipelines/training_pipeline.py
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -7,7 +7,7 @@ from src.DimondPricePrediction.exception import CustomException
 from src.DimondPricePrediction.components.data_ingestion import DataIngestion
 from src.DimondPricePrediction.components.data_transformation import DataTransformation
 from src.DimondPricePrediction.components.model_trainer import ModelTrainer
-
+from src.DimondPricePrediction.components.model_evaluation import ModelEvaluation
 obj=DataIngestion()
 
 train_data_path,test_data_path=obj.initiate_data_ingestion()
@@ -17,3 +17,6 @@ train_arr , test_arr = data_transformation.initialize_data_transformation(train_
 
 model = ModelTrainer()
 model.initate_model_training(train_arr,test_arr)
+
+model_eval_obj = ModelEvaluation()
+model_eval_obj.initiate_model_evaluation(train_arr,test_arr)
